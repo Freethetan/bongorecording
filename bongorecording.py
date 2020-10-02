9a7dd71d82883822c9f581fa4d8bc4885e0d4e7c
Viktor Honchar <viktor.gonchar@gmail.com>
2020-10-01 22:31:00 -0400

    Search file by '_screenshare' and then replacing '_screenshawe' will geve us an audio file

--- bongorecording.py
+++ bongorecording.py
@@ -18,31 +18,40 @@ USAGE:
         print(" otput will be "+fname+fext)
 
 def mergecontent(vpath,apath,outpath):
+        print (outpath)
         vinput = ffmpeg.input(vpath)
         ainput = ffmpeg.input(apath)
         (
                 ffmpeg
-                .concat(vinput, ainput)
+		.input(vinput)
+                .concat(vinput, ainput,v=1,a=1)
                 .output(outpath)
                 .run()
         )
 
 def getcontentfiles(path):
-        files=[];
+        files=["",""];
         for filename in os.listdir(videofile):
                 if filename.endswith(fext):
-                        files.append(os.path.join(videofile,filename))
+                       if filename.find("_screenshare.mp4") != -1:
+                                files[0]=os.path.join(videofile,filename)
+                                files[1]=os.path.join(videofile,filename.replace("_screenshare","")+".mp4")
+                                break;
         return files
 if len(sys.argv) == 1:
         helpinfo();
         sys.exit();
 
 videofile = sys.argv[1]
+
+
 '''Change into subdirectory in case it is a top directory'''
 if os.path.isdir(videofile) and os.path.isdir(os.path.join(videofile,"meetingFiles")):
         videofile = os.path.join(videofile,"meetingFiles");
 '''Get bongo files that contain video and audio'''
 contfiles = getcontentfiles(videofile)
+
+
 '''Get next lecture number'''
 prefix="";
 if os.path.isfile(fname+fext):
@@ -51,4 +60,4 @@ if os.path.isfile(fname+fext):
                 num+=1;
         prefix=str(num)
 
-mergecontent(contfiles[0],contfiles[1],fname+str(prefix)+fext)
+mergecontent(contfiles[0],contfiles[1],str(fname+str(prefix)+fext))
